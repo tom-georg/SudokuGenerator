3f8282354623af4927d149bd739cfc0d504ac5bf
tom-georg-mes <wahhef-merJos-4vyjxi>
2020-01-23 17:45:49 +0100

    More comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here....
+
         System.out.println("Hello World! :D");
+        // Everyone is free to add some sysouts or comments here.
+        // We will introduce discipline later.
     }
 }
