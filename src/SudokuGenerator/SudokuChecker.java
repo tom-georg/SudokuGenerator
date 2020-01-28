f28745c11a007c6762286a5f1fc269d67a556798
Thomas Georg <thomas.georg85@gmail.com>
2020-01-28 15:35:20 +0100

    Neues Package mit neuer Klasse und neuen Tests

--- src/SudokuGenerator/SudokuChecker.java
+++ src/SudokuGenerator/SudokuChecker.java
@@ -3,4 +3,26 @@ package SudokuGenerator;
 public class SudokuChecker {
 
 
+    /**
+     *
+     * @param board (the sudoku board)
+     * @param x
+     * @param y
+     * @return
+     *     true:  The vertical and horizontal lines of the board
+     *            are valid: No duplicates
+     *     false: There are duplicates in the vertical and
+     *            horizonal lines
+     *
+     *   Example:
+     *   board:          x:1 y:0
+     *        213
+     *        121
+     *        322
+     *   returns false because 213 (horizonal) is valid, 122 (vertical) is not.
+     */
+    public boolean checkLines(int[][] board, int x, int y) {
+
+        return false;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,29 +5,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World! :D");
-        for(int i = 0; i<10;i++){
-            System.out.println(i);
-        }
-
-        Fenster f = new Fenster("TEST");
-
-
-       /* Random r = new Random();
-        int a = r.nextInt(4);
-        int uno = r.nextInt(1001);
-        int dos = r.nextInt(1001);
-        if(a == 0) {int zero = uno + dos;}
-        if(a == 1) {int one = uno - dos;}
-        if(a == 2) {int two = uno * dos;}
-        if(a == 2) {
-            if(uno > dos){
-                int three = uno / dos;
-            }
-            else{
-                int three = dos / uno;
-            }*/
-        }
-        //
 
     }
+
+
+}
