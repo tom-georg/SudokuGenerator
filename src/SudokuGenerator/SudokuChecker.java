dd58b3165339fd1de29d14fc92cf83ad934a0a3a
JanRuessing <jan.ruessing.schule@gmail.com>
2020-01-30 21:10:06 +0100

    Horizontales Testen funktioniert, !vertikales fehlt!

--- src/SudokuGenerator/SudokuChecker.java
+++ src/SudokuGenerator/SudokuChecker.java
@@ -23,6 +23,21 @@ public class SudokuChecker {
      *   returns false because 213 (horizonal) is valid, 122 (vertical) is not.
      */
     public boolean checkLines(int[][] board, int x, int y) {
+        //Ich hole zuerst eine Kopie der ersten Zeile,
+        // dann hake ich in einer boolean Array die Ziffern ab,
+        // welche in der ersten Zeile vorkommen.
+        //Sollte schon eine Ziffer abgehakt sein, returne ich false.
+        boolean[] checklist = new boolean[board.length];
+
+        for (int i = 0; i < board.length; i++) {
+            int[] checkoffhorizontal = board[i];
+            for (int j = 0; j < checkoffhorizontal.length; j++) {
+                if( checklist[checkoffhorizontal[j]] = true){
+                    return false;
+                }
+                checklist[checkoffhorizontal[j]] = true;
+            }
+        }
 
         return false;
     }
