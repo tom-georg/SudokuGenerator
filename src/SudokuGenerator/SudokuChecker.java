18470bfc7adff2140edef657a2fd1d649d6a1e97
Leander Thias <louvilp@googlemail.com>
2020-02-07 15:22:14 +0100

    SudokuGenerator angefangen. Er generiert bis jetzt eine Box mit korrekten Zahlen und gibt diese über die Konsole zurück. Diese Box wird noch nicht in das am Anfang der Methode erstellte Array eingefügt.

--- src/SudokuGenerator/SudokuChecker.java
+++ src/SudokuGenerator/SudokuChecker.java
@@ -5,31 +5,14 @@ import static java.lang.Math.ceil;
 public class SudokuChecker {
 
 
-    /**
-     * The method checklines
-     * *
-     * @param board (the sudoku board)
-     * @param x position
-     * @param y position
-     * @return
-     *     true:  The vertical and horizontal lines of the board
-     *            are valid: No duplicates
-     *     false: There are duplicates in the vertical and
-     *            horizonal lines
-     *
-     *   Example:
-     *   board:          x:1 y:0
-     *        213
-     *        121
-     *        322
-     *   returns false because 213 (horizonal) is valid, 122 (vertical) is not.
-     *
-     */
+
     public boolean checkLines(int[][] board, int x, int y) {
         //Checkt alle Zahlen entlang der x-Achse
         boolean[] checklist = new boolean[board.length];    //Eine Array die immer an der Position einer bestimmten Zahl auf true gesetzt wird
         for(int i = 0 ; i < board.length ; i ++){
-            if(checklist[board[i][y]-1] == true){
+            if(board[i][y] > 8){ //Alle Zahlen über 8 werden ignoriert, da dies das generieren eines Feldes erleichtert
+
+            }else if(checklist[board[i][y]-1] == true){
                 return false;
             }else{
                 checklist[board[i][y]-1] = true;
@@ -38,7 +21,9 @@ public class SudokuChecker {
         //Checkt alle Zahlen entlang der y-Achse
         checklist = new boolean[board[x].length];
         for(int i = 0 ; i < board[x].length ; i ++){
-            if(checklist[board[x][i]-1] == true){
+            if(board[x][i] > 8){ //Alle Zahlen über 8 werden ignoriert, da dies das generieren eines Feldes erleichtert
+
+            }else if(checklist[board[x][i]-1] == true){
                 return false;
             }else{
                 checklist[board[i][y]-1] = true;
@@ -47,14 +32,16 @@ public class SudokuChecker {
         return true;
     }
 
-    public boolean checkBox(int[][] board, int x, int y) {
+    public boolean checkBox(int[][] board, int x, int y) {  //Unnötigste Methode wo gibt
         int[] box = getBox(x, y);
         x = box[0];
         y = box[1];
         boolean[] checklist = new boolean[board.length];    //Eine Array die immer an der Position einer bestimmten Zahl auf true gesetzt wird
         for(int i = y ; i < y+3 ; i ++){
             for(int j = x ; j < x+3 ; j ++){
-                if(checklist[board[i][j]-1] == true){
+                if(board[i][j] > 8){ //Alle Zahlen über 8 werden ignoriert, da dies das generieren eines Feldes erleichtert
+
+                }else if(checklist[board[i][j]-1] == true){
                     return false;
                 }else{
                     checklist[board[i][j]-1] = true;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 import SudokuGenerator.SudokuChecker;
+import SudokuGenerator.SudokuGenerator;
 
 import java.util.*;
 
@@ -8,69 +9,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        //Erstellt ein 2D array, voller Zahlen
-        int[][] board = new int[9][9];
-        for(int i = 0 ; i < board.length ; i ++){
-            for(int j = 0 ; j < board[i].length ; j ++){
-                board[i][j] = i+1;
-            }
-        }
-        //In der 3. Spalte werden die Zahlen verändert, sodass nur unterschiedliche Zahlen in der Spalte sind
-        for(int i = 0 ; i < board[2].length ; i ++){
-            board[2][i] = i+1;
-        }
-
-        //In der 1. Box werden die Zahlen so verändert, sodass die Box richtig ist
-        board[0][0] = 1;
-        board[1][0] = 2;
-        board[2][0] = 3;
-        board[0][1] = 4;
-        board[1][1] = 5;
-        board[2][1] = 6;
-        board[0][2] = 7;
-        board[1][2] = 8;
-        board[2][2] = 9;
-
-        //Gibt das board in der Konsole aus;
-        String output = "";
-        for(int i = 0 ; i < board.length ; i ++){
-            output = "";
-            for(int j = 0 ; j < board[i].length ; j ++){
-                output += " " + board[i][j];
-            }
-            System.out.println(output);
-        }
-
-        SudokuChecker sChecker = new SudokuChecker();
-        System.out.println(sChecker.checkBox(board, 8, 8));
-
-
-
-
-        //Fenster f = new Fenster("TEST");
-
-
-
-       /* Random r = new Random();
-        int a = r.nextInt(4);
-        int uno = r.nextInt(1001);
-        int dos = r.nextInt(1001);
-        if(a == 0) {int zero = uno + dos;}
-        if(a == 1) {int one = uno - dos;}
-        if(a == 2) {int two = uno * dos;}
-        if(a == 2) {
-            if(uno > dos){
-                int three = uno / dos;
-            }
-            else{
-                int three = dos / uno;
-            }*/
-        }
-        //test 24.01.2020
-        // Everyone is free to add some sysouts or comments here.
-        // We will introduce discipline later.
-      // Test 2 :D
-        // Test 3 Laurin
-      // UPDATE: 27.01.2020 15:30 Uhr -
-
+        SudokuGenerator sGen = new SudokuGenerator();
+        sGen.generateField();
     }
+}
